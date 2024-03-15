bbe82859d3a841121f519fe076fa4722f128b6c9
Atala2000 <atalakidi@gmail.com>
2024-03-16 01:00:17 +0300

    8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,21 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Function that returns a function
+    Args:
+        multiplier (int): digit to multiply
+    Returns:
+        function
+    """
+
     def multiply(value: float) -> float:
+        """
+        Returns a multiplied value
+        Args:
+            value (int): Digit to mulitply
+        Returns: float
+        """
         return value * multiplier
 
     return multiply
