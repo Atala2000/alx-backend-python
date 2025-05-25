1e8e344cffc1544b493431510b68e50956cb119b
Kidiavayi Atala <kidiavayi@buupass.com>
2025-05-25 12:44:44 +0300

    Refactor database connection handling and simplify async fetch functions

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -3,26 +3,6 @@ import aiosqlite
 import asyncio
 from sqlite3 import Error
 
-class DataBaseConnection:
-
-    @staticmethod
-    async def connect_database():
-        try:
-            db = await aiosqlite.connect('user_alx.db')
-            async with db.cursor() as cursor:
-                await cursor.execute(
-                    """
-                    CREATE TABLE IF NOT EXISTS users (
-                        username TEXT,
-                        age INTEGER,
-                        email TEXT
-                    )
-                    """
-                )
-                await db.commit()
-            return db
-        except Error as e:
-            print(f"Connection error: {e}")
 
 async def async_fetch_users(db):
     async with db.execute("SELECT * FROM users") as cursor:
@@ -30,19 +10,20 @@ async def async_fetch_users(db):
         return results
 
 
-async def async_fetch_older_users(db):
+async def async_fetch_older_users():
     async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
         results = await cursor.fetchall()
         return results
+    
+results = await asyncio.gather(
+    async_fetch_users(),
+    async_fetch_older_users()
+)
 
 async def fetch_concurrently():
-    db = await DataBaseConnection.connect_database()
     
     # Run both queries at once
-    results = await asyncio.gather(
-        DataBaseConnection.async_fetch_users(db),
-        DataBaseConnection.async_fetch_older_users(db)
-    )
+
 
     print("All users:", results[0])
     print("Older users:", results[1])
