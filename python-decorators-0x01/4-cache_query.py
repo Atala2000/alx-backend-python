8cc102e7abd0568cf4b0bd6f8799070c4f277ff3
Kidiavayi Atala <kidiavayi@buupass.com>
2025-05-25 14:20:13 +0300

    Make script executable by changing file permissions

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import time
+import sqlite3 
+import functools
+
+
+query_cache = {}
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    return wrapper
+
+
+def cache_query(func):
+    def wrapper(*args, **kwargs):
+        print(kwargs.get('query'))
+        results = query_cache.get(kwargs.get('query'))
+        if not results:
+            results = func(*args, **kwargs)
+            print(results)
+
+        return results
+    return wrapper
+
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+#### First call will cache the result
+users = fetch_users_with_cache(query="SELECT * FROM users")
+
+#### Second call will use the cached result
+users_again = fetch_users_with_cache(query="SELECT * FROM users")
