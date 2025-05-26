9fa8aa2d48eb1894900570792630eb54bb3d3ad8
Kidiavayi Atala <kidiavayi@buupass.com>
2025-05-26 14:35:59 +0300

    Add REST framework configuration for authentication and permissions

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -52,6 +52,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'messaging_app.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
