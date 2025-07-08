e16e0cfa55fdfc3c0dec3e0fa12ff92e07787d02
Kidiavayi Atala <kidiavayi@buupass.com>
2025-07-08 14:10:32 +0300

    refactor: improve request logging in middleware and add log file

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,16 +1,24 @@
 from datetime import datetime
+import logging
 
 
 class RequestLoggingMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
+        logging.basicConfig(
+            filename="requests.log",
+            level=logging.INFO
+        )
 
     def __call__(self, request):
         
         user = request.user
-
         print(f"{datetime.now()} - User: {user} - Path: {request.path}")
+
+        logging.info(
+            f"{datetime.now()} - User: {user} - Path: {request.path}"
+        )
 
         response = self.get_response(request)
 
